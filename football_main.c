798e32e1aae19ff34efe933f66e08b8118477c6e
CarRuff <143749217+CarRuff@users.noreply.github.com>
2025-03-25 23:06:52 -0500

    Update football_main.c

--- football_main.c
+++ football_main.c
@@ -1,5 +1,27 @@
 #include "football.h"
 
-int main(int argc, char *argv[]) {
+// Main Function 
+int main() {
+    // Print pre-text
+    std::cout<<"Enter 0 or 1 to STOP\n";
+    
+    // Declare score
+    int score;
+    
+    // Get Points from user
+    std::cout << "Enter points:";
+    std::cin >> score;
+
+    // If the user inputed 1 or less
+    if (score <= 1) {
+        // Exit 
+        return 0;
+    // If the user inputed 1 or more
+    } else {
+        // Call find with score
+        find(score);
+    }
+
+    // Program Success
     return 0;
 }
